9cf627701fbc6db8c1b31060182340a79c30d72f
vinhNguyen <beminhquan3@gmail.com>
2023-02-14 15:16:05 +0700

    add image games

--- lib/app/pages/games/games_page.dart
+++ lib/app/pages/games/games_page.dart
@@ -1,3 +1,5 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,6 +13,54 @@ class GamesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GameWidget(
+      game: MyGame(),
+    );
+  }
+}
+
+class MyGame extends FlameGame {
+  SpriteComponent girl = SpriteComponent();
+  SpriteComponent boy = SpriteComponent();
+  SpriteComponent background = SpriteComponent();
+  final double charaterSize = 400;
+  bool boyFilp = true;
+  @override
+  onLoad() async {
+    super.onLoad();
+    final screenWidth = size[0];
+    final screenHeight = size[1];
+    const textBoxHeight = 100;
+    add(background
+      ..sprite = await loadSprite('background.jpg')
+      ..size = size);
+    girl
+      ..sprite = await loadSprite('girl.png')
+      ..size = Vector2(charaterSize, charaterSize)
+      ..y = screenHeight / 2 - charaterSize / 2 - textBoxHeight
+      ..anchor = Anchor.topCenter;
+    boy
+      ..sprite = await loadSprite('boy.png')
+      ..size = Vector2(charaterSize, charaterSize)
+      ..y = screenHeight / 2 - charaterSize / 2 - textBoxHeight
+      ..x = screenWidth - charaterSize
+      ..anchor = Anchor.topCenter
+      ..flipHorizontally();
+    addAll([girl, boy]);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (girl.x < size.x / 2 - 150) {
+      girl.x += dt * 150;
+    } else {
+      if (boy.isFlippedHorizontally) {
+        boy.flipHorizontally();
+      }
+    }
+    if (boy.x > size.x / 2 - 50) {
+      boy.x -= dt * 150;
+    }
   }
 }
